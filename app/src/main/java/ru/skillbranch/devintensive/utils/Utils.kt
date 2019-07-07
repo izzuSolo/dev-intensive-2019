e3cf8bf665b4faa5a7807ce504bc163b1b5a7575
izzuSoloQ <skat-senin@yandex.ru>
2019-07-07 10:10:41 +0300

    transLiteral

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -9,8 +9,79 @@ object Utils {
         return firstName to lastName
     }
     fun transliteration(payload: String, divider: String = " "): String{
-        return payload
+        return payload.toCharArray().map{character -> if(character.isUpperCase()) transliteralCharacter(character.toLowerCase()).capitalize()
+            else transliteralCharacter(character)}.joinToString("").replace(" ", divider)
+        }
+
+       fun transliteralCharacter(character: Char): String = when(character){
+        'а'-> "a"
+
+        'б'-> "b"
+
+        'в'-> "v"
+
+        'г'-> "g"
+
+        'д'-> "d"
+
+        'е'-> "e"
+
+        'ё'-> "e"
+
+        'ж'-> "zh"
+
+        'з'-> "z"
+
+        'и'-> "i"
+
+        'й'-> "i"
+
+        'к'-> "k"
+
+        'л'-> "l"
+
+        'м'-> "m"
+
+        'н'-> "n"
+
+        'о'-> "o"
+
+        'п'-> "p"
+
+        'р'-> "r"
+
+        'с'-> "s"
+
+        'т'-> "t"
+
+        'у'-> "u"
+
+        'ф'-> "f"
+
+        'х'-> "h"
+
+        'ц'-> "c"
+
+        'ч'-> "ch"
+
+        'ш'-> "sh"
+
+        'щ'-> "sh'"
+
+        'ъ'-> ""
+
+        'ы'-> "i"
+
+        'ь'-> ""
+
+        'э'-> "e"
+
+        'ю'-> "yu"
+
+        'я'-> "ya"
+        else -> character.toString()
     }
+
 
     fun toInitials(firstName: String?, lastName: String?): String?{
         val firstChar: String = firstName?.capitalize()?.getOrNull(0).toString()
