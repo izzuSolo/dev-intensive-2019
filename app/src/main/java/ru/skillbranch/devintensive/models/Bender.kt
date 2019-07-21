be37d23911edb0f690fe39297aac28e7fecd09f8
izzuSoloQ <skat-senin@yandex.ru>
2019-07-21 17:54:12 +0300

    task 6

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -14,14 +14,24 @@ class Bender(var status:Status = Status.NORMAL, var question: Question = Questio
 
     fun listenAnswer(answer:String):Pair<String, Triple<Int, Int, Int>>{
 
-        return if(question.answer.contains(answer)){
-            question = question.nextQuestion()
-            "Отлично - ты справился\n${question.question}" to status.color
-        }else{
-            status = status.nextStatus()
-            "Это неправильный ответ\n${question.question}" to status.color
-        }
+        return validateAnswer(answer) to status.color
     }
+
+    fun validateAnswer(answer:String) :String = if(question.answer.contains(answer.toLowerCase())){
+        if(question.validate(answer).equals("Отлично - ты справился")){
+            question = question.nextQuestion()
+            "Отлично - ты справился\n${question.question}"
+        } else {
+            "${question.validate(answer)}\n${question.question}"
+        }
+    } else if(status == Status.CRITICAL){
+        status = Status.NORMAL
+        "Это неправильный ответ. Давай все по новой\n${Question.NAME.question}"
+    } else {
+        status = status.nextStatus()
+        "Это неправильный ответ\n${question.question}"
+    }
+
 
     enum class Status(val color: Triple<Int, Int, Int>){
         NORMAL(Triple(255,255,255)),
@@ -40,23 +50,40 @@ class Bender(var status:Status = Status.NORMAL, var question: Question = Questio
     enum class Question(val question: String, val answer: List<String>){
         NAME("Как меня зовут?", listOf("бендер","bender")) {
             override fun nextQuestion(): Question = PROFESSION
+            override fun validate(answer: String): String =
+                if(answer.trim().firstOrNull()?.isUpperCase()?:false){"Отлично - ты справился"}
+                else {"Имя должно начинаться с заглавной буквы"}
         },
         PROFESSION("Назови мою профессию?", listOf("сгибальщик","bender")){
             override fun nextQuestion(): Question = MATERIAL
+            override fun validate(answer: String): String =
+                if(!(answer.trim().firstOrNull()?.isUpperCase()?: false)){"Отлично - ты справился"}
+                else {"Профессия должна начинаться со строчной буквы"}
         },
         MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron","wood")){
             override fun nextQuestion(): Question = BDAY
+            override fun validate(answer: String): String =
+                if(answer.trim().matches(Regex("\\d+")).not()){"Отлично - ты справился"}
+                else {"Материал не должен содержать цифр"}
         },
         BDAY("Когда меня создали?", listOf("2993")){
             override fun nextQuestion(): Question = SERIAL
+            override fun validate(answer: String): String =
+                if(answer.trim().matches(Regex("^[0-9]*$"))){"Отлично - ты справился"}
+                else {"Год моего рождения должен содержать только цифры"}
         },
         SERIAL("Мой серийный номер?", listOf("2716057")){
             override fun nextQuestion(): Question = IDLE
+            override fun validate(answer: String): String =
+                if(answer.trim().matches(Regex("^[0-9]*$")) && answer.length == 7) {"Отлично - ты справился"}
+                else {"Серийный номер содержит только цифры, и их 7"}
         },
-        IDLE("На этом все. вопросов больше нет", listOf("бендер","bender")){
+        IDLE("На этом все, вопросов больше нет", listOf("бендер","bender")){
             override fun nextQuestion(): Question = IDLE
+            override fun validate(answer: String): String = ""
         };
 
         abstract fun nextQuestion():Question
+        abstract fun validate(answer: String): String
     }
 }
