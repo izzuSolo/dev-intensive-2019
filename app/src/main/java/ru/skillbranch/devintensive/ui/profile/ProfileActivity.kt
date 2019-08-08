fd688fc6832d0b7e663719e6d8d4a27f82fa6a36
izzuSoloQ <skat-senin@yandex.ru>
2019-08-08 22:04:10 +0300

    resume

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -1,21 +1,16 @@
 package ru.skillbranch.devintensive.ui.profile
 
 
-import android.graphics.Color
+import android.graphics.ColorFilter
 import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.KeyEvent
 import android.view.View
-import android.view.inputmethod.EditorInfo
 import android.widget.EditText
-import android.widget.ImageView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_profile.*
 import ru.skillbranch.devintensive.R
-import ru.skillbranch.devintensive.models.Bender
-import ru.skillbranch.devintensive.extensions.hideKeyboard
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -35,9 +30,14 @@ class ProfileActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
+        outState.putBoolean(IS_EDIT_MODE, isEditMode)
     }
 
     private fun initViews(savedInstanceState: Bundle?) {
+
+        isEditMode = savedInstanceState?.getBoolean(IS_EDIT_MODE, false) ?: false
+        showCurrentMode(isEditMode)
+
         viewFields = mapOf(
             "nickname" to tv_nick_name,
             "rank" to tv_rank,
@@ -58,7 +58,37 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     private fun showCurrentMode(isEdit: Boolean) {
-        val info = viewFields.filter {setOf("firstName", "lastName", "about", "repository").contains(it.key)}
+        val info = viewFields.filter { setOf("firstName", "lastName", "about", "repository").contains(it.key) }
+
+        for ((_, v) in info) {
+            v as EditText
+            v.isFocusable = isEdit
+            v.isFocusableInTouchMode = isEdit
+            v.isEnabled = isEdit
+            v.background.alpha = if (isEdit) 255 else 0
+        }
+
+        ic_eye.visibility = if (isEdit) View.GONE else View.VISIBLE
+        wr_about.isCounterEnabled = isEdit
+
+        with(btn_edit){
+            val filter: ColorFilter? = if(isEdit){
+                PorterDuffColorFilter(
+                    resources.getColor(R.color.color_accent,theme),
+                    PorterDuff.Mode.SRC_IN
+                )
+            } else {
+                null
+            }
+
+            val icon = if(isEdit){
+                resources.getDrawable(R.drawable.ic_save_black_24dp)
+            } else {
+                resources.getDrawable(R.drawable.ic_edit_black_24dp)
+            }
+            background.colorFilter = filter
+            setImageDrawable(icon)
+        }
     }
 }
 
